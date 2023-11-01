cd5d41849027a4475432ce49702e0a3cb0ea7378
AMIN AKHTER <120783238+Aminakhter1@users.noreply.github.com>
2023-11-01 12:48:37 +0530

    Update RomanToInteger.java
    I solved this problem without collections

--- RomanToInteger.java
+++ RomanToInteger.java
@@ -1,43 +1,36 @@
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Scanner;
-
 public class RomanToInteger {
-    public static int romanToInt(String s) {
-        Map<Character, Integer> romanValues = new HashMap<>();
-        romanValues.put('I', 1);
-        romanValues.put('V', 5);
-        romanValues.put('X', 10);
-        romanValues.put('L', 50);
-        romanValues.put('C', 100);
-        romanValues.put('D', 500);
-        romanValues.put('M', 1000);
-
-        int result = 0;
-        int prevValue = 0;
-
-        for (int i = s.length() - 1; i >= 0; i--) {
-            int currentValue = romanValues.get(s.charAt(i));
-
-            if (currentValue < prevValue) {
-                result -= currentValue;
-            } else {
-                result += currentValue;
-            }
-
-            prevValue = currentValue;
-        }
-
-        return result;
-    }
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a Roman numeral: ");
-        String romanNumeral = scanner.nextLine();
+        String roman = scanner.nextLine();
         scanner.close();
+        int result = romanToInteger(roman);
+        System.out.println("Roman numeral " + roman + " is equivalent to integer " + result);
+    }
 
-        int integerEquival = romanToInt(romanNumeral);
-        System.out.println("The integer equivalent of " + romanNumeral + " is " + integerEquival);
+    public static int romanToInteger(String s) {
+        int result = 0;
+        int prevValue = 0; // Keep track of the previous character's value
+       // Create a mapping of Roman numerals to their integer values
+        int[] values = new int[26];
+        values['I' - 'A'] = 1;
+        values['V' - 'A'] = 5;
+        values['X' - 'A'] = 10;
+        values['L' - 'A'] = 50;
+        values['C' - 'A'] = 100;
+        values['D' - 'A'] = 500;
+        values['M' - 'A'] = 1000;
+        // Iterate through the Roman numeral string from right to left
+        for (int i = s.length() - 1; i >= 0; i--) {
+            int value = values[s.charAt(i) - 'A'];
+             if (value < prevValue) {
+                result -= value; // Subtraction rule (e.g., IV or IX)
+            } else {
+                result += value; // Addition rule (e.g., I, V, X)
+            }
+            prevValue = value;
+        }
+        return result;
     }
 }
